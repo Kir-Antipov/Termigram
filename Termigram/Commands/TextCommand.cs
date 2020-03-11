db1e5f85524a7954649fcc9e1cc3ee93329b56b6
Kir-Antipov <kp_antipov@mail.ru>
2020-03-11 11:51:46 +0200

    Implemented a command class with string parameters

---
+++ Termigram/Commands/TextCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Telegram.Bot.Types;
+
+namespace Termigram.Commands
+{
+    public class TextCommand : ParametricCommandBase<string>
+    {
+        #region Init
+        public TextCommand(string name, Update update, params string[] parameters) : this(name, update, (IEnumerable<string>)parameters) { }
+
+        public TextCommand(string name, Update update, IEnumerable<string> parameters) : base(name, update, new List<string>(parameters).AsReadOnly()) { }
+        #endregion
+    }
+}
