90a630c40c4a68db6d4582b0646db855a93c5283
Kir-Antipov <kp_antipov@mail.ru>
2020-03-11 11:41:27 +0200

    Made interfaces for parametric commands

---
+++ Termigram/Commands/IParametricCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Termigram.Commands
+{
+    public interface IParametricCommand : ICommand
+    {
+        IReadOnlyList<object?> Parameters { get; }
+    }
+}
---
+++ Termigram/Commands/IParametricCommand`1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Termigram.Commands
+{
+    public interface IParametricCommand<out TParameter> : IParametricCommand where TParameter : class
+    {
+        new IReadOnlyList<TParameter?> Parameters { get; }
+
+        IReadOnlyList<object?> IParametricCommand.Parameters => Parameters;
+    }
+}
