08c424d3ff06ef4d8852a1d1b496341bfd4b33ac
Kir-Antipov <kp_antipov@mail.ru>
2020-03-11 11:53:22 +0200

    Made an attribute to mark command methods

---
+++ Termigram/Commands/CommandAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Termigram.Commands
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class CommandAttribute : Attribute
+    {
+        #region Var
+        public string Name { get; }
+        #endregion
+
+        #region Init
+        public CommandAttribute(string name) => Name = name;
+        #endregion
+
+        #region Functions
+        public override string ToString() => Name;
+        public override int GetHashCode() => HashCode.Combine(base.GetHashCode(), Name);
+        public override bool Equals(object? obj) => obj is CommandAttribute attribute && Name == attribute.Name;
+        #endregion
+    }
+}
