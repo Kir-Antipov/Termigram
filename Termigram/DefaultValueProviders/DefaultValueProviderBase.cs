86b13127ca9e40a140df2aa44ca4cc5ed966045c
Kir-Antipov <kp_antipov@mail.ru>
2020-03-12 15:53:00 +0200

    Made base implementation for IDefaultValueProvider

---
+++ Termigram/DefaultValueProviders/DefaultValueProviderBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Termigram.DefaultValueProviders
+{
+    public abstract class DefaultValueProviderBase : IDefaultValueProvider
+    {
+        public abstract bool TryProvideDefaultValue(ParameterInfo parameter, out object? value);
+    }
+}
