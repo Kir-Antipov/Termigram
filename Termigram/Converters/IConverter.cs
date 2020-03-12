6facde2bad710abad651292d21f657f80f6c0f0a
Kir-Antipov <kp_antipov@mail.ru>
2020-03-12 14:21:20 +0200

    Created interfaces for converters

---
+++ Termigram/Converters/IConverter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Termigram.Converters
+{
+    public interface IConverter
+    {
+        bool TryConvert(object? value, Type resultType, out object? result);
+    }
+}
---
+++ Termigram/Converters/IConverter`2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Termigram.Converters
+{
+    public interface IConverter<in TInput, TOutput> : IConverter
+    {
+        bool TryConvert([AllowNull]TInput value, [MaybeNull]out TOutput result);
+
+        bool IConverter.TryConvert(object? value, Type resultType, out object? result)
+        {
+            if (resultType.IsAssignableFrom(typeof(TOutput)) && value is TInput input && TryConvert(input, out TOutput output))
+            {
+                result = output;
+                return true;
+            }
+            else
+            {
+                result = default;
+                return false;
+            }
+        }
+    }
+}
