31627d8a6fefe94b424afe0165e70d56253b8e71
Kir-Antipov <kp_antipov@mail.ru>
2020-03-12 14:42:36 +0200

    Implemented default converter

---
+++ Termigram/Converters/DefaultConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Termigram.Converters
+{
+    public class DefaultConverter : ConverterBase
+    {
+        public override bool TryConvert(object? value, Type resultType, out object? result)
+        {
+            try
+            {
+                if (resultType.IsAssignableFrom(value?.GetType()))
+                {
+                    result = value;
+                    return true;
+                }
+                else
+                {
+                    result = Convert.ChangeType(value, resultType);
+                    return true;
+                }
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
+        }
+    }
+}
