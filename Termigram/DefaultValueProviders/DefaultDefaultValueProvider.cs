af47b5f61f6a04067960c5e9f0938ed5ce1cc979
Kir-Antipov <kp_antipov@mail.ru>
2020-03-12 15:58:27 +0200

    Implemented default IDefaultValueProvider
    (Yep, it's not a typo ¯\_(ツ)_/¯)

---
+++ Termigram/DefaultValueProviders/DefaultDefaultValueProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using Termigram.Extensions;
+
+namespace Termigram.DefaultValueProviders
+{
+    public class DefaultDefaultValueProvider : DefaultValueProviderBase
+    {
+        public override bool TryProvideDefaultValue(ParameterInfo parameter, out object? value)
+        {
+            if (parameter.HasDefaultValue)
+            {
+                value = parameter.DefaultValue;
+                return true;
+            }
+
+            Type resultType = parameter.ParameterType;
+            if (resultType.IsValueType)
+            {
+                try
+                {
+                    value = Activator.CreateInstance(resultType);
+                    return true;
+                }
+                catch
+                {
+                    value = default;
+                    return false;
+                }
+            }
+            else
+            {
+                if (parameter.CanBeNull())
+                {
+                    value = default;
+                    return true;
+                }
+
+                if (resultType == typeof(string))
+                {
+                    value = string.Empty;
+                    return true;
+                }
+
+                try
+                {
+                    value = Activator.CreateInstance(resultType);
+                    return true;
+                }
+                catch
+                {
+                    value = default;
+                    return false;
+                }
+            }
+        }
+    }
+}
