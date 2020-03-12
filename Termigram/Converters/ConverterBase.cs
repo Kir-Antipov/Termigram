dfff51ff8ffcba320f7deb3f97953621ae051094
Kir-Antipov <kp_antipov@mail.ru>
2020-03-12 14:33:05 +0200

    Made base implementations for IConverter interfaces

---
+++ Termigram/Converters/ConverterBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Termigram.Converters
+{
+    public abstract class ConverterBase : IConverter
+    {
+        public abstract bool TryConvert(object? value, Type resultType, out object? result);
+    }
+}
---
+++ Termigram/Converters/ConverterBase`2.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Termigram.Converters
+{
+    public abstract class ConverterBase<TInput, TOutput> : IConverter<TInput, TOutput>
+    {
+        public abstract bool TryConvert([AllowNull]TInput value, [MaybeNull]out TOutput result);
+    }
+}
