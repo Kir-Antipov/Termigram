1e24d8d324bc8a60aa43a0a18de3d9bc6a7a7e17
Kir-Antipov <kp_antipov@mail.ru>
2020-03-13 18:38:35 +0200

    Made an extension class for Type

---
+++ Termigram/Extensions/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Termigram.Extensions
+{
+    internal static class TypeExtensions
+    {
+        public static IEnumerable<T> LoadAllImplementations<T>() => Assembly.GetExecutingAssembly().ExportedTypes
+            .Where(typeof(T).IsAssignableFrom)
+            .Where(x => !x.IsAbstract && !x.IsInterface)
+            .Select(x =>
+            {
+                ConstructorInfo constructor = x.GetConstructor(Type.EmptyTypes);
+                return constructor is null ? default : (T)constructor.Invoke(Array.Empty<object>());
+            })
+            .Where(x => x is { })
+            .ToArray();
+
+        public static T[] LoadAllImplementationsAsArray<T>() => LoadAllImplementations<T>().ToArray();
+
+        public static IReadOnlyList<T> LoadAllImplementationsAsReadOnlyList<T>() => LoadAllImplementations<T>().ToList().AsReadOnly();
+    }
+}
