ca2623d36a7c703fd62a93bdfcf524cebab3a325
Kir-Antipov <kp_antipov@mail.ru>
2020-03-13 18:16:05 +0200

    Made an extension class for ParameterInfo

---
+++ Termigram/Extensions/ParameterInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Termigram.Extensions
+{
+    internal static class ParameterInfoExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool CanBeNull(this ParameterInfo parameter) => parameter.CustomAttributes.Any(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute" && x.ConstructorArguments.Count == 1 && x.ConstructorArguments[0].Value is byte b && b == 2);
+    }
+}
