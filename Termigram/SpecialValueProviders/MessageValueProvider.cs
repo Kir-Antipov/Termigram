f85bd790513c28bd3fa31a0325d8fbac53af23d2
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 12:16:46 +0200

    Made ISpecialValueProvider for Message type

---
+++ Termigram/SpecialValueProviders/MessageValueProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Telegram.Bot.Types;
+using Termigram.Commands;
+using Termigram.Extensions;
+
+namespace Termigram.SpecialValueProviders
+{
+    public class MessageValueProvider : SpecialValueProviderBase<Message?>
+    {
+        public override bool TryProvideSpecialValue(ParameterInfo parameter, ICommand command, out Message? value) =>
+            command.Update.TryGetMessage(out value) || parameter.CanBeNull();
+    }
+}
