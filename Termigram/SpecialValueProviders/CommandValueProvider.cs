eb7f51f94c73cd9a3ba31de67ea7ecbbd4c63d91
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 12:25:54 +0200

    Implemented ISpecialValueProvider for ICommand type

---
+++ Termigram/SpecialValueProviders/CommandValueProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public class CommandValueProvider : SpecialValueProviderBase<ICommand>
+    {
+        protected override ICommand ProvideSpecialValue(ParameterInfo parameter, ICommand command) => command;
+    }
+}
