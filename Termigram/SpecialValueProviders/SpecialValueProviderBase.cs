a5ea44b9e088cc212abc4fa4b09de8e054abf8be
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 11:56:50 +0200

    Made base implementations for ISpecialValueProvider interfaces

---
+++ Termigram/SpecialValueProviders/SpecialValueProviderBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public abstract class SpecialValueProviderBase : ISpecialValueProvider
+    {
+        public virtual bool TryProvideSpecialValue(ParameterInfo parameter, ICommand command, out object? value)
+        {
+            try
+            {
+                value = ProvideSpecialValue(parameter, command);
+                return true;
+            }
+            catch
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        protected virtual object? ProvideSpecialValue(ParameterInfo parameter, ICommand command) => default;
+    }
+}
---
+++ Termigram/SpecialValueProviders/SpecialValueProviderBase`1.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public abstract class SpecialValueProviderBase<TValue> : ISpecialValueProvider<TValue>
+    {
+        public virtual bool TryProvideSpecialValue(ParameterInfo parameter, ICommand command, [MaybeNull]out TValue value)
+        {
+            try
+            {
+                value = ProvideSpecialValue(parameter, command);
+                return true;
+            }
+            catch
+            {
+                value = default!;
+                return false;
+            }
+        }
+
+        [return: MaybeNull]
+        protected virtual TValue ProvideSpecialValue(ParameterInfo parameter, ICommand command) => default!;
+    }
+}
