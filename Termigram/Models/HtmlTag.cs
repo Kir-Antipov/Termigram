3091db8213a53668e07352b95b481bfec78d4d45
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 11:41:46 +0200

    Made an internal model for representing the html tag

---
+++ Termigram/Models/HtmlTag.cs
@@ -0,0 +1,24 @@
+﻿namespace Termigram.Models
+{
+    internal class HtmlTag
+    {
+        #region Var
+        public readonly string Open;
+        public readonly string Close;
+        #endregion
+
+        #region Init
+        public HtmlTag(string tag)
+        {
+            Open = $"<{tag}>";
+            Close = $"</{tag}>";
+        }
+
+        public static implicit operator HtmlTag(string tag) => new HtmlTag(tag);
+        #endregion
+
+        #region Functions
+        public override string ToString() => Open;
+        #endregion
+    }
+}
