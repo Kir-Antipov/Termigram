f573f640b625b45ebe4aeeb420e51f3b754e9373
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 17:30:53 +0200

    Made interface for command invocation

---
+++ Termigram/CommandInvokers/ICommandInvoker.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandInvokers
+{
+    public interface ICommandInvoker
+    {
+        object? Invoke(ICommand command, MethodInfo method);
+
+        object? Invoke(ICommand command, MethodInfo method, object target);
+    }
+}
