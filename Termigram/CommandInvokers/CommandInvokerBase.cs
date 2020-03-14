806827d08d1e3f4bd9b77ec83007dae92bdfc3fe
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 17:37:54 +0200

    Made base implementation for ICommandInvoker

---
+++ Termigram/CommandInvokers/CommandInvokerBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandInvokers
+{
+    public abstract class CommandInvokerBase : ICommandInvoker
+    {
+        public object? Invoke(ICommand command, MethodInfo method) => InvokeImpl(command, method, null);
+        public object? Invoke(ICommand command, MethodInfo method, object target) => InvokeImpl(command, method, target);
+
+        protected abstract object? InvokeImpl(ICommand command, MethodInfo method, object? target);
+    }
+}
