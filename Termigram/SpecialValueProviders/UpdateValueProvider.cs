85bbd2654601c8c108a29a6d1c6a2ee181902438
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 12:34:22 +0200

    Implemented ISpecialValueProvider for Update type

---
+++ Termigram/SpecialValueProviders/UpdateValueProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using Telegram.Bot.Types;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public class UpdateValueProvider : SpecialValueProviderBase<Update>
+    {
+        protected override Update ProvideSpecialValue(ParameterInfo parameter, ICommand command) => command.Update;
+    }
+}
