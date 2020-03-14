db46c9bd933cb0cf6f75efd21b3e06c60089bc85
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 11:52:18 +0200

    Made an extension class for ParseMode

---
+++ Termigram/Extensions/ParseModeExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Telegram.Bot.Types.Enums;
+using Termigram.Models;
+
+namespace Termigram.Extensions
+{
+    public static class ParseModeExtensions
+    {
+        private static readonly HtmlTag[] HtmlTags = new HtmlTag[]
+        {
+            "a",
+            "i", "em",
+            "u", "ins",
+            "pre", "code",
+            "b", "strong",
+            "s", "strike", "del"
+        };
+
+        private static readonly MarkdownTag[] MarkdownTags = new MarkdownTag[]
+        {
+            "*",
+            "~",
+            "_",
+            "`",
+            new MarkdownTag("[", "]", "(", ")")
+        };
+
+        public static ParseMode RecognizeSuitableParseMode(string? text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return ParseMode.Default;
+
+            for (int i = 0; i < HtmlTags.Length; ++i)
+                if (text.Contains(HtmlTags[i].Open) || text.Contains(HtmlTags[i].Close))
+                    return ParseMode.Html;
+
+            for (int i = 0; i < MarkdownTags.Length; ++i)
+                if (MarkdownTags[i].IsMatch(text))
+                    return ParseMode.MarkdownV2;
+
+            return ParseMode.Default;
+        }
+    }
+}
