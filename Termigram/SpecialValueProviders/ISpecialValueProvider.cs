6bded37b7952f3125648e9d2f6efa74057c3807a
Kir-Antipov <kp_antipov@mail.ru>
2020-03-14 11:55:17 +0200

    Made interfaces to provide default values

---
+++ Termigram/SpecialValueProviders/ISpecialValueProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public interface ISpecialValueProvider
+    {
+        bool TryProvideSpecialValue(ParameterInfo parameter, ICommand command, out object? value);
+    }
+}
---
+++ Termigram/SpecialValueProviders/ISpecialValueProvider`1.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.SpecialValueProviders
+{
+    public interface ISpecialValueProvider<TValue> : ISpecialValueProvider
+    {
+        bool TryProvideSpecialValue(ParameterInfo parameter, ICommand command, [MaybeNull]out TValue value);
+
+        bool ISpecialValueProvider.TryProvideSpecialValue(ParameterInfo parameter, ICommand command, out object? value)
+        {
+            if (parameter.ParameterType.IsAssignableFrom(typeof(TValue)) && TryProvideSpecialValue(parameter, command, out TValue typedValue))
+            {
+                value = typedValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+    }
+}
