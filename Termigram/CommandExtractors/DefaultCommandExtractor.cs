728a3899a4315ce05604ae8441160a62b9f27449
Kir-Antipov <kp_antipov@mail.ru>
2020-03-15 13:21:46 +0200

    Implemented default ICommandExtractor

---
+++ Termigram/CommandExtractors/DefaultCommandExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandExtractors
+{
+    public class DefaultCommandExtractor : CommandExtractorBase
+    {
+        public override MethodInfo[] ExtractCommands(Type botType) => botType
+            .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+            .Select(x => new { Method = x, CommandName = x.GetCustomAttribute<CommandAttribute>()?.Name })
+            .Where(x => !string.IsNullOrWhiteSpace(x.CommandName))
+            .GroupBy(x => x.CommandName)
+            .Select(x => x.First().Method)
+            .ToArray();
+    }
+}
