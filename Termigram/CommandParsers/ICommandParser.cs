8c9872b98c5fbb946dad4b4ac2f288ac01ebaa95
Kir-Antipov <kp_antipov@mail.ru>
2020-03-15 16:50:47 +0200

    Created interfaces for parsing commands from updates

---
+++ Termigram/CommandParsers/ICommandParser.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types;
+using Termigram.Commands;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Termigram.CommandParsers
+{
+    public interface ICommandParser
+    {
+        bool TryParse(Update update, [NotNullWhen(true)]out ICommand? command);
+    }
+}
---
+++ Termigram/CommandParsers/ICommandParser`1.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Telegram.Bot.Types;
+using Termigram.Commands;
+
+namespace Termigram.CommandParsers
+{
+    public interface ICommandParser<TCommand> : ICommandParser where TCommand : class, ICommand
+    {
+        bool TryParse(Update update, [NotNullWhen(true)]out TCommand? command);
+
+        bool ICommandParser.TryParse(Update update, [NotNullWhen(true)]out ICommand? command)
+        {
+            bool parsed = TryParse(update, out TCommand? typedCommand);
+            command = typedCommand;
+            return parsed;
+        }
+    }
+}
