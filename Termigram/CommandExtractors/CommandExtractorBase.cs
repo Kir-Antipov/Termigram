a241fd47a85a272f228b0e9748e6c2364da02a48
Kir-Antipov <kp_antipov@mail.ru>
2020-03-15 13:16:17 +0200

    Made base implementation for ICommandExtractor

---
+++ Termigram/CommandExtractors/CommandExtractorBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+namespace Termigram.CommandExtractors
+{
+    public abstract class CommandExtractorBase : ICommandExtractor
+    {
+        public abstract MethodInfo[] ExtractCommands(Type botType);
+    }
+}
