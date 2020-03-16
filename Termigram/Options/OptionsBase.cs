94ccf6e50857f09baa1b47fd748872ea85f714dd
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:46:14 +0200

    Made base implementation for IState interface

---
+++ Termigram/Options/OptionsBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Telegram.Bot.Types.Enums;
+using Termigram.CommandExtractors;
+using Termigram.CommandInvokers;
+using Termigram.CommandLinkers;
+using Termigram.CommandParsers;
+using Termigram.ResultProcessors;
+
+namespace Termigram.Options
+{
+    public abstract class OptionsBase : IOptions
+    {
+        #region Var
+        public abstract string Token { get; }
+        public abstract TimeSpan CancellationCheckInterval { get; }
+        public abstract HttpClient? HttpClient { get; }
+        public abstract IWebProxy? Proxy { get; }
+        public abstract ICommandExtractor CommandExtractor { get; }
+        public abstract IReadOnlyList<UpdateType>? AllowedUpdates { get; }
+        public abstract IReadOnlyList<ICommandParser> Parsers { get; }
+        public abstract IReadOnlyList<ICommandLinker> Linkers { get; }
+        public abstract ICommandInvoker CommandInvoker { get; }
+        public abstract IReadOnlyList<IResultProcessor> ResultProcessors { get; }
+        #endregion
+
+        #region Functions
+        public override string ToString() => Token;
+        public override int GetHashCode() => Token.GetHashCode();
+        public override bool Equals(object? obj) => obj is IOptions options && Token == options.Token;
+        #endregion
+    }
+}
