4b31e0598d33af8644d7fe748f4a2540e1653c6f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:40:19 +0200

    Implemented sending of plain chat actions

---
+++ Termigram/ResultProcessors/ChatActionProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+
+namespace Termigram.ResultProcessors
+{
+    public class ChatActionProcessor : ResultProcessorBase<ChatAction>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, ChatAction result) =>
+            bot.Client.SendChatActionAsync(command.Update.GetChatIdOrUserId(), result);
+    }
+}
