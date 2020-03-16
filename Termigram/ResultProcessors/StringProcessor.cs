beddf4ae2acbaf6fc5b4923ac83f9d4fad7975d9
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:43:46 +0200

    Implemented sending of text messages (as plain string)

---
+++ Termigram/ResultProcessors/StringProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class StringProcessor : ResultProcessorBase<string>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, string result)
+        {
+            TextMessage message = result;
+
+            return bot.Client.SendTextMessageAsync(command.Update.GetChatIdOrUserId(), message.Text, message.ParseMode, message.DisableWebPagePreview, message.DisableNotification, message.ReplyToMessageId, message.ReplyMarkup);
+        }
+    }
+}
