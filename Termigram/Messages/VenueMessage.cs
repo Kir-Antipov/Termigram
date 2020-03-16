7eebc8ba1c9170c47642935b51cfd5b4f554b66f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:43:57 +0200

    Implemented sending of venues

---
+++ Termigram/Messages/VenueMessage.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class VenueMessage
+    {
+		#region Var
+		public float Latitude { get; }
+		public float Longitude { get; }
+		public string Title { get; }
+		public string Address { get; }
+		public string? FoursquareId { get; }
+		public string? FoursquareType { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public VenueMessage(float latitude, float longitude, string title, string address, string? foursquareId = null, string? foursquareType = null, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+		{
+			Latitude = latitude;
+			Longitude = longitude;
+			Title = title;
+			Address = address;
+			FoursquareId = foursquareId;
+			FoursquareType = foursquareType;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/VenueMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class VenueMessageProcessor : ResultProcessorBase<VenueMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, VenueMessage result) =>
+            bot.Client.SendVenueAsync(command.Update.GetChatIdOrUserId(), result.Latitude, result.Longitude, result.Title, result.Address, result.FoursquareId, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.FoursquareType);
+    }
+}
