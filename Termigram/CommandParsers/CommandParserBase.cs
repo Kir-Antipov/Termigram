c4a5734e3a6ddaceeae6c032c34c716fba1f48af
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:18:16 +0200

    Made base implementations for ICommandParser interfaces

---
+++ Termigram/CommandParsers/CommandParserBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Telegram.Bot.Types;
+using Termigram.Commands;
+
+namespace Termigram.CommandParsers
+{
+    public abstract class CommandParserBase : ICommandParser
+    {
+        public abstract bool TryParse(Update update, [NotNullWhen(true)]out ICommand? command);
+    }
+}
---
+++ Termigram/CommandParsers/CommandParserBase`1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Telegram.Bot.Types;
+using Termigram.Commands;
+
+namespace Termigram.CommandParsers
+{
+    public abstract class CommandParserBase<TCommand> : ICommandParser<TCommand> where TCommand : class, ICommand
+    {
+        public abstract bool TryParse(Update update, [NotNullWhen(true)]out TCommand? command);
+    }
+}
