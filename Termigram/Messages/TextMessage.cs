0e45c934b9b980ba6d51de0259dc0f9a796a2fdd
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:43:19 +0200

    Implemented sending of text messages

---
+++ Termigram/Messages/TextMessage.cs
@@ -0,0 +1,39 @@
+﻿using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Termigram.Extensions;
+
+namespace Termigram.Messages
+{
+    public class TextMessage
+    {
+        #region Var
+        public string Text { get; }
+        public ParseMode ParseMode { get; }
+        public bool DisableWebPagePreview { get; }
+        public bool DisableNotification { get; }
+        public int ReplyToMessageId { get; }
+        public IReplyMarkup? ReplyMarkup { get; }
+        #endregion
+
+        #region Init
+        public TextMessage(string text, ParseMode? parseMode = null, bool disableWebPagePreview = false, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+        {
+            Text = text;
+            ParseMode = parseMode ?? ParseModeExtensions.RecognizeSuitableParseMode(text);
+            DisableWebPagePreview = disableWebPagePreview;
+            DisableNotification = disableNotification;
+            ReplyToMessageId = replyToMessageId;
+            ReplyMarkup = replyMarkup;
+        }
+        #endregion
+
+        #region Functions
+        public static implicit operator string(TextMessage message) => message.Text;
+        public static implicit operator TextMessage(string text) => new TextMessage(text);
+
+        public override string ToString() => Text;
+        public override int GetHashCode() => Text.GetHashCode();
+        public override bool Equals(object? obj) => obj is TextMessage message && Text == message.Text;
+        #endregion
+    }
+}
---
+++ Termigram/ResultProcessors/TextMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class TextMessageProcessor : ResultProcessorBase<TextMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, TextMessage result) =>
+            bot.Client.SendTextMessageAsync(command.Update.GetChatIdOrUserId(), result.Text, result.ParseMode, result.DisableWebPagePreview, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup);
+    }
+}
