6e2834c46c199c28b7a0799c459ce5a0ac6cfcae
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:39:23 +0200

    Implemented sending of audio files

---
+++ Termigram/Messages/AudioMessage.cs
@@ -0,0 +1,40 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Termigram.Extensions;
+
+namespace Termigram.Messages
+{
+	public class AudioMessage
+    {
+		#region Var
+		public InputOnlineFile Audio { get; }
+		public string? Caption { get; }
+		public ParseMode ParseMode { get; }
+		public int Duration { get; }
+		public string? Performer { get; }
+		public string? Title { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		public InputMedia? Thumb { get; }
+		#endregion
+
+		#region Init
+		public AudioMessage(InputOnlineFile audio, string? caption = null, ParseMode? parseMode = null, int duration = 0, string? performer = null, string? title = null, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null, InputMedia? thumb = null)
+		{
+			Audio = audio;
+			Caption = caption;
+			ParseMode = parseMode ?? ParseModeExtensions.RecognizeSuitableParseMode(caption);
+			Duration = duration;
+			Performer = performer;
+			Title = title;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+			Thumb = thumb;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/AudioMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class AudioMessageProcessor : ResultProcessorBase<AudioMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, AudioMessage result) =>
+            bot.Client.SendAudioAsync(command.Update.GetChatIdOrUserId(), result.Audio, result.Caption, result.ParseMode, result.Duration, result.Performer, result.Title, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.Thumb);
+    }
+}
