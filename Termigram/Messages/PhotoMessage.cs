aa6ed20122efac56d9d04d95cc2b9ea696aec837
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:42:26 +0200

    Implemented sending of photos

---
+++ Termigram/Messages/PhotoMessage.cs
@@ -0,0 +1,31 @@
+﻿using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Termigram.Extensions;
+
+namespace Termigram.Messages
+{
+	public class PhotoMessage
+    {
+		#region Var
+		public InputOnlineFile Photo { get; }
+		public string? Caption { get; }
+		public ParseMode ParseMode { get; set; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public PhotoMessage(InputOnlineFile photo, string? caption = null, ParseMode? parseMode = null, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+		{
+			Photo = photo;
+			Caption = caption;
+			ParseMode = parseMode ?? ParseModeExtensions.RecognizeSuitableParseMode(caption);
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/PhotoMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class PhotoMessageProcessor : ResultProcessorBase<PhotoMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, PhotoMessage result) =>
+            bot.Client.SendPhotoAsync(command.Update.GetChatIdOrUserId(), result.Photo, result.Caption, result.ParseMode, result.DisableNotification, result.ReplyToMessageId);
+    }
+}
