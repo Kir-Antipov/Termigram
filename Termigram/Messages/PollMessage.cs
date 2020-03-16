89cff4d489612358bc6c4a4df54587135039b202
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:42:45 +0200

    Implemented sending of polls

---
+++ Termigram/Messages/PollMessage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class PollMessage
+    {
+		#region Var
+		public string Question { get; }
+		public IEnumerable<string> Options { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		public bool? IsAnonymous { get; }
+		public PollType? Type { get; }
+		public bool? AllowsMultipleAnswers { get; }
+		public int? CorrectOptionId { get; }
+		public bool? IsClosed { get; }
+		#endregion
+
+		#region Init
+		public PollMessage(string question, IEnumerable<string> options, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null, bool? isAnonymous = null, PollType? type = null, bool? allowsMultipleAnswers = null, int? correctOptionId = null, bool? isClosed = null)
+		{
+			Question = question;
+			Options = options;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+			IsAnonymous = isAnonymous;
+			Type = type;
+			AllowsMultipleAnswers = allowsMultipleAnswers;
+			CorrectOptionId = correctOptionId;
+			IsClosed = isClosed;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/PollMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class PollMessageProcessor : ResultProcessorBase<PollMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, PollMessage result) =>
+            bot.Client.SendPollAsync(command.Update.GetChatIdOrUserId(), result.Question, result.Options, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.IsAnonymous, result.Type, result.AllowsMultipleAnswers, result.CorrectOptionId, result.IsClosed);
+    }
+}
