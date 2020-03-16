e381d57842800c07459269a836c44f9c340b9c9f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:44:09 +0200

    Implemented sending of videos

---
+++ Termigram/Messages/VideoMessage.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Termigram.Extensions;
+
+namespace Termigram.Messages
+{
+	public class VideoMessage
+    {
+		#region Var
+		public InputOnlineFile Video { get; }
+		public int Duration { get; }
+		public int Width { get; }
+		public int Height { get; }
+		public InputMedia? Thumb { get; }
+		public string? Caption { get; }
+		public ParseMode ParseMode { get; }
+		public bool SupportsStreaming { get; }
+		public int ReplyToMessageId { get; }
+		public bool DisableNotification { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public VideoMessage(InputOnlineFile video, int duration = 0, int width = 0, int height = 0, InputMedia? thumb = null, string? caption = null, ParseMode? parseMode = null, bool supportsStreaming = false, int replyToMessageId = 0, bool disableNotification = false, IReplyMarkup? replyMarkup = null)
+		{
+			Video = video;
+			Duration = duration;
+			Width = width;
+			Height = height;
+			Thumb = thumb;
+			Caption = caption;
+			ParseMode = parseMode ?? ParseModeExtensions.RecognizeSuitableParseMode(caption);
+			SupportsStreaming = supportsStreaming;
+			ReplyToMessageId = replyToMessageId;
+			DisableNotification = disableNotification;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/VideoMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class VideoMessageProcessor : ResultProcessorBase<VideoMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, VideoMessage result) =>
+            bot.Client.SendVideoAsync(command.Update.GetChatIdOrUserId(), result.Video, result.Duration, result.Width, result.Height, result.Caption, result.ParseMode, result.SupportsStreaming, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.Thumb);
+    }
+}
