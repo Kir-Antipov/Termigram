57e8c07db40e00618493beeb1f05fb4bd710a266
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:44:24 +0200

    Implemented sending of video notes

---
+++ Termigram/Messages/VideoNoteMessage.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class VideoNoteMessage
+    {
+		#region Var
+		public InputTelegramFile VideoNote { get; }
+		public int Duration { get; }
+		public int Length { get; }
+		public InputMedia? Thumb { get; }
+		public int ReplyToMessageId { get; }
+		public bool DisableNotification { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public VideoNoteMessage(InputTelegramFile videoNote, int duration = 0, int length = 0, InputMedia? thumb = null, int replyToMessageId = 0, bool disableNotification = false, IReplyMarkup? replyMarkup = null)
+		{
+			VideoNote = videoNote;
+			Duration = duration;
+			Length = length;
+			Thumb = thumb;
+			ReplyToMessageId = replyToMessageId;
+			DisableNotification = disableNotification;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/VideoNoteMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class VideoNoteMessageProcessor : ResultProcessorBase<VideoNoteMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, VideoNoteMessage result) =>
+            bot.Client.SendVideoNoteAsync(command.Update.GetChatIdOrUserId(), result.VideoNote, result.Duration, result.Length, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.Thumb);
+    }
+}
