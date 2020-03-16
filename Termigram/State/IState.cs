8ea62fb886da104b16bdf95072ed275d1871034f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:30:43 +0200

    Made interface for accessing bot's state

---
+++ Termigram/State/IState.cs
@@ -0,0 +1,11 @@
+﻿namespace Termigram.State
+{
+    public interface IState
+    {
+        void SetState(long id, object? state);
+
+        bool TryGetState(long id, out object? state);
+
+        bool TryTakeState(long id, out object? state);
+    }
+}
