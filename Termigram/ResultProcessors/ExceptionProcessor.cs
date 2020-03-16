2349f2a496ece734ac20bd73651f25e51108c92d
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:41:23 +0200

    Implemented sending of error messages

---
+++ Termigram/ResultProcessors/ExceptionProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class ExceptionProcessor : ResultProcessorBase<Exception>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, Exception result)
+        {
+            TextMessage message = result switch
+            {
+                AggregateException aggregate => aggregate.InnerException.Message,
+                TargetInvocationException targetInvocation => targetInvocation.InnerException.Message,
+                _ => result.Message
+            };
+
+            return bot.Client.SendTextMessageAsync(command.Update.GetChatIdOrUserId(), message.Text, message.ParseMode, message.DisableWebPagePreview, message.DisableNotification, message.ReplyToMessageId, message.ReplyMarkup);
+        }
+    }
+}
