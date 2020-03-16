94fba63fef0bba2ab3ba558fc30ff4406a3cb63b
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:39:49 +0200

    Implemented sending of chat actions

---
+++ Termigram/Messages/ChatActionMessage.cs
@@ -0,0 +1,20 @@
+﻿using Telegram.Bot.Types.Enums;
+
+namespace Termigram.Messages
+{
+	public class ChatActionMessage
+    {
+		#region Var
+		public ChatAction ChatAction { get; }
+		#endregion
+
+		#region Init
+		public ChatActionMessage(ChatAction chatAction) => ChatAction = chatAction;
+		#endregion
+
+		#region Functions
+		public static implicit operator ChatAction(ChatActionMessage message) => message.ChatAction;
+		public static implicit operator ChatActionMessage(ChatAction chatAction) => new ChatActionMessage(chatAction);
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/ChatActionMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class ChatActionMessageProcessor : ResultProcessorBase<ChatActionMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, ChatActionMessage result) =>
+            bot.Client.SendChatActionAsync(command.Update.GetChatIdOrUserId(), result.ChatAction);
+    }
+}
