b88e2c8214970c14ddc0f57729a642ef626a2bb5
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:40:38 +0200

    Implemented sending of contacts

---
+++ Termigram/Messages/ContactMessage.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class ContactMessage
+    {
+		#region Var
+		public string PhoneNumber { get; }
+		public string Firstname { get; }
+		public string? Lastname { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		public string? VCard { get; }
+		#endregion
+
+		#region Init
+		public ContactMessage(string phoneNumber, string firstname, string? lastname = null, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null, string? vCard = null)
+		{
+			PhoneNumber = phoneNumber;
+			Firstname = firstname;
+			Lastname = lastname;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+			VCard = vCard;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/ContactMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class ContactMessageProcessor : ResultProcessorBase<ContactMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, ContactMessage result) =>
+            bot.Client.SendContactAsync(command.Update.GetChatIdOrUserId(), result.PhoneNumber, result.Firstname, result.Lastname, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.VCard);
+    }
+}
