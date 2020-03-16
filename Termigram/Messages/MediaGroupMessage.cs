af5e6e804fd5ba50ec4ff00259849708c91eaad0
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:42:11 +0200

    Implemented sending of media groups

---
+++ Termigram/Messages/MediaGroupMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Telegram.Bot.Types;
+
+namespace Termigram.Messages
+{
+	public class MediaGroupMessage
+    {
+		#region Var
+		public IEnumerable<IAlbumInputMedia> MediaGroup { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		#endregion
+
+		#region Init
+		public MediaGroupMessage(params IAlbumInputMedia[] mediaGroup) : this((IEnumerable<IAlbumInputMedia>)mediaGroup) { }
+
+		public MediaGroupMessage(IEnumerable<IAlbumInputMedia> mediaGroup, bool disableNotification = false, int replyToMessageId = 0)
+		{
+			MediaGroup = mediaGroup;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/MediaGroupMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class MediaGroupMessageProcessor : ResultProcessorBase<MediaGroupMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, MediaGroupMessage result) =>
+            bot.Client.SendMediaGroupAsync(result.MediaGroup, command.Update.GetChatIdOrUserId(), result.DisableNotification, result.ReplyToMessageId);
+    }
+}
