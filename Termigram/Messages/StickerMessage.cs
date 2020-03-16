f06576e30aef9dc338fcd03f20b8ed7aa615ed6c
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:42:59 +0200

    Implemented sending of stickers

---
+++ Termigram/Messages/StickerMessage.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class StickerMessage
+    {
+		#region Var
+		public InputOnlineFile Sticker { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public StickerMessage(InputOnlineFile sticker, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+		{
+			Sticker = sticker;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/StickerMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class StickerMessageProcessor : ResultProcessorBase<StickerMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, StickerMessage result) =>
+            bot.Client.SendStickerAsync(command.Update.GetChatIdOrUserId(), result.Sticker, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup);
+    }
+}
