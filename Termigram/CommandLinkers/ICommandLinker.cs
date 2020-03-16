e3c7560465260d7c889ade2fca8f2b91de1634db
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:23:17 +0200

    Made interfaces for linking commands with methods

---
+++ Termigram/CommandLinkers/ICommandLinker.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandLinkers
+{
+    public interface ICommandLinker
+    {
+        bool CanBeLinked(ICommand command, MethodInfo method);
+    }
+}
---
+++ Termigram/CommandLinkers/ICommandLinker`1.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandLinkers
+{
+    public interface ICommandLinker<in TCommand> : ICommandLinker where TCommand : ICommand
+    {
+        bool CanBeLinked(TCommand command, MethodInfo method);
+
+        bool ICommandLinker.CanBeLinked(ICommand command, MethodInfo method) => command is TCommand typedCommand && CanBeLinked(typedCommand, method);
+    }
+}
