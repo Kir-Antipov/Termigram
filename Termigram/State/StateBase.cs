9e99800e8fb580aa82b7fee40003483c4ff92255
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:31:02 +0200

    Made base implementation for IState interface

---
+++ Termigram/State/StateBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Termigram.State
+{
+    public abstract class StateBase : IState
+    {
+        public abstract void SetState(long id, object? state);
+
+        public abstract bool TryGetState(long id, out object? state);
+
+        public abstract bool TryTakeState(long id, out object? state);
+    }
+}
