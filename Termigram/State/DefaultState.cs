b9218453c1774b28f1be21671bfbab5f093bb97f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:31:26 +0200

    Implemented default IState based on ConcurrentDictionary

---
+++ Termigram/State/DefaultState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace Termigram.State
+{
+    public class DefaultState : StateBase
+    {
+        #region Var
+        private readonly ConcurrentDictionary<long, object?> Storage = new ConcurrentDictionary<long, object?>();
+        #endregion
+
+        #region Functions
+        public override void SetState(long id, object? state) => Storage.AddOrUpdate(id, state, (a, b) => state);
+
+        public override bool TryGetState(long id, out object? state) => Storage.TryGetValue(id, out state);
+
+        public override bool TryTakeState(long id, out object? state) => Storage.TryRemove(id, out state);
+        #endregion
+    }
+}
