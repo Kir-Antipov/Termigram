b3139be0ddaeb5755e75fddada5b8205896a7cb6
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:41:53 +0200

    Implemented sending of locations

---
+++ Termigram/Messages/LocationMessage.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class LocationMessage
+    {
+		#region Var
+		public float Latitude { get; }
+		public float Longitude { get; }
+		public int LivePeriod { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		#endregion
+
+		#region Init
+		public LocationMessage(float latitude, float longitude, int livePeriod = 0, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+		{
+			Latitude = latitude;
+			Longitude = longitude;
+			LivePeriod = livePeriod;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+		}
+		#endregion
+	}
+}
---
+++ Termigram/ResultProcessors/LocationMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class LocationMessageProcessor : ResultProcessorBase<LocationMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, LocationMessage result) =>
+            bot.Client.SendLocationAsync(command.Update.GetChatIdOrUserId(), result.Latitude, result.Longitude, result.LivePeriod, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup);
+    }
+}
