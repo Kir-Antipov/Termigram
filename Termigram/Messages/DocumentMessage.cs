4fa62f6fab09693d47ecc53948cdc5d6548c3b79
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:40:56 +0200

    Implemented sending of documents

---
+++ Termigram/Messages/DocumentMessage.cs
@@ -0,0 +1,35 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Termigram.Extensions;
+
+namespace Termigram.Messages
+{
+	public class DocumentMessage
+    {
+		#region Var
+		public InputOnlineFile Document { get; }
+		public string? Caption { get; }
+		public ParseMode ParseMode { get; set; }
+		public int ReplyToMessageId { get; }
+		public bool DisableNotification { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+		public InputMedia? Thumb { get; }
+		#endregion
+
+		#region Init
+		public DocumentMessage(InputOnlineFile document, string? caption = null, ParseMode? parseMode = null, int replyToMessageId = 0, bool disableNotification = false, IReplyMarkup? replyMarkup = null, InputMedia? thumb = null)
+		{
+			Document = document;
+			Caption = caption;
+			ParseMode = parseMode ?? ParseModeExtensions.RecognizeSuitableParseMode(caption);
+			ReplyToMessageId = replyToMessageId;
+			DisableNotification = disableNotification;
+			ReplyMarkup = replyMarkup;
+			Thumb = thumb;
+		}
+		#endregion
+
+	}
+}
---
+++ Termigram/ResultProcessors/DocumentMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class DocumentMessageProcessor : ResultProcessorBase<DocumentMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, DocumentMessage result) =>
+            bot.Client.SendDocumentAsync(command.Update.GetChatIdOrUserId(), result.Document, result.Caption, result.ParseMode, result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup, default, result.Thumb);
+    }
+}
