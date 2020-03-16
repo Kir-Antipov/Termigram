c0b893683361fd3202661202f9b2cb5840c468b9
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:47:04 +0200

    Implemented default IStateOptions

---
+++ Termigram/Options/DefaultStateOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using Termigram.State;
+
+namespace Termigram.Options
+{
+    public class DefaultStateOptions : DefaultOptions, IStateOptions
+    {
+        #region Var
+        public IState State { get; }
+        #endregion
+
+        #region Init
+        public DefaultStateOptions(string token) : this(token, default, default) { }
+
+        public DefaultStateOptions(string token, HttpClient httpClient) : this(token, httpClient, default) { }
+
+        public DefaultStateOptions(string token, IWebProxy proxy) : this(token, default, proxy) { }
+
+        protected DefaultStateOptions(string token, HttpClient? httpClient, IWebProxy? proxy) : base(token, httpClient, proxy)
+        {
+            State = new DefaultState();
+        }
+        #endregion
+    }
+}
