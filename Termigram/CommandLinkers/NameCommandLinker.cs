01aa69c21f2fcefbf946719ba55e10b9dbc57150
Kir-Antipov <kp_antipov@mail.ru>
2020-03-16 18:25:41 +0200

    Implemented default ICommandLinker

---
+++ Termigram/CommandLinkers/NameCommandLinker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.CommandLinkers
+{
+    public class NameCommandLinker : CommandLinkerBase
+    {
+        public override bool CanBeLinked(ICommand command, MethodInfo method)
+        {
+            string? name = method.GetCustomAttribute<CommandAttribute>()?.Name;
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            return command.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
