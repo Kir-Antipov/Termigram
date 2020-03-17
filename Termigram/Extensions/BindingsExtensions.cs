7b00102b85d6f9fb9a542f2e40fcd76e28c29b91
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:52:14 +0200

    Made an extension class for Bindings

---
+++ Termigram/Extensions/BindingsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Termigram.Commands;
+
+namespace Termigram.Extensions
+{
+    public static class BindingsExtensions
+    {
+        public static BindingFlags ExtractBindingFlags(this Bindings bindings)
+        {
+            BindingFlags result = 0;
+
+            if (bindings.HasFlag(Bindings.Public))
+                result |= BindingFlags.Public;
+
+            if (bindings.HasFlag(Bindings.NonPublic))
+                result |= BindingFlags.NonPublic;
+
+            if (bindings.HasFlag(Bindings.Instance))
+                result |= BindingFlags.Instance;
+
+            if (bindings.HasFlag(Bindings.Static))
+                result |= BindingFlags.Static;
+
+            if (!bindings.HasFlag(Bindings.Inherited))
+                result |= BindingFlags.DeclaredOnly;
+
+            return result;
+        }
+    }
+}
