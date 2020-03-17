fd180e0f59cf89b98338851ca8907be9e915b50f
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:50:47 +0200

    Added Bindings enum to describe command extraction strategy

---
+++ Termigram/Commands/Bindings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Termigram.Commands
+{
+    [Flags]
+    public enum Bindings
+    {
+        None            = 0,
+
+        Public          = 1 << 0,
+        NonPublic       = 1 << 1,
+        Instance        = 1 << 2,
+        Static          = 1 << 3,
+        MarkedAsCommand = 1 << 4,
+        Inherited       = 1 << 5,
+        
+        Any = Public | NonPublic | Instance | Static,
+        AnyStatic = Public | NonPublic | Static,
+        AnyInstance = Public | NonPublic | Instance,
+        Default = Public | NonPublic | Instance | Static | MarkedAsCommand
+    }
+}
