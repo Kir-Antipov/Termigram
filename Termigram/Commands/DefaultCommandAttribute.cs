4c75cd8ef740b17309ffd9ab489fc32c5f09f503
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:51:26 +0200

    Added attribute to mark default command

---
+++ Termigram/Commands/DefaultCommandAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Termigram.Commands
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class DefaultCommandAttribute : Attribute
+    {
+        #region Var
+        public string? Name { get; }
+        #endregion
+
+        #region Init
+        public DefaultCommandAttribute() => Name = default;
+
+        public DefaultCommandAttribute(string name) => Name = name;
+        #endregion
+    }
+}
