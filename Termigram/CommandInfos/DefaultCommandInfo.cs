ca15556c0bb0a9211039df6aa3ddf960fe535fee
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:48:54 +0200

    Implemented default ICommandInfo

---
+++ Termigram/CommandInfos/DefaultCommandInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Termigram.CommandInfos
+{
+    public class DefaultCommandInfo : CommandInfoBase
+    {
+        #region Var
+        public override string Name { get; }
+
+        public override MethodInfo Method { get; }
+        #endregion
+
+        #region Init
+        public DefaultCommandInfo(string name, MethodInfo method)
+        {
+            Name = name;
+            Method = method;
+        }
+        #endregion
+    }
+}
