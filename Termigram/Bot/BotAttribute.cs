1dcfd1a22eea454a0493903fe94c70f3b285c705
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:53:27 +0200

    Made BotAttribute for default command extraction strategy overriding

---
+++ Termigram/Bot/BotAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Termigram.Commands;
+
+namespace Termigram.Bot
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class BotAttribute : Attribute
+    {
+		#region Var
+		public Bindings Bindings { get; }
+		#endregion
+
+		#region Init
+		public BotAttribute(Bindings bindings = Bindings.Default) => Bindings = bindings;
+		#endregion
+	}
+}
