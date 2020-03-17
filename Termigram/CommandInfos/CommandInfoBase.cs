b0dd4462842ff5449e22f8bc5bc7745d872ff4d4
Kir-Antipov <kp_antipov@mail.ru>
2020-03-17 17:48:36 +0200

    Made base implementation for ICommandInfo interface

---
+++ Termigram/CommandInfos/CommandInfoBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace Termigram.CommandInfos
+{
+    public abstract class CommandInfoBase : ICommandInfo
+    {
+		#region Var
+        public abstract string Name { get; }
+
+        public abstract MethodInfo Method { get; }
+        #endregion
+
+        #region Functions
+        public override string ToString() => Name;
+        public override int GetHashCode() => Name.GetHashCode();
+        public override bool Equals(object? obj) => obj is ICommandInfo command && Name == command.Name;
+        #endregion
+    }
+}
