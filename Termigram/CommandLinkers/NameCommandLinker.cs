49c566c6e0c280430b997a9859a3b17d0e17a260
Kir-Antipov <kp_antipov@mail.ru>
2020-03-18 17:34:47 +0200

    Updated default ICommandLinker. Now it supports several command names

--- Termigram/CommandLinkers/NameCommandLinker.cs
+++ Termigram/CommandLinkers/NameCommandLinker.cs
@@ -6,7 +6,13 @@ namespace Termigram.CommandLinkers
 {
     public class NameCommandLinker : CommandLinkerBase
     {
-        public override bool CanBeLinked(ICommand command, ICommandInfo commandInfo) =>
-            command.Name.Equals(commandInfo.Name, StringComparison.InvariantCultureIgnoreCase);
+        public override bool CanBeLinked(ICommand command, ICommandInfo commandInfo)
+        {
+            for (int i = 0; i < commandInfo.Names.Count; ++i)
+                if (command.Name.Equals(commandInfo.Names[i], StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+
+            return false;
+        }
     }
 }
