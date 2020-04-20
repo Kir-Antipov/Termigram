840d8151ae3b0f78682f90a4600f9e3fe435d936
Kir-Antipov <kp_antipov@mail.ru>
2020-04-20 20:27:41 +0300

    Implemented wrappers for SendDiceAsync

---
+++ Termigram/Messages/DiceMessage.cs
@@ -0,0 +1,23 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Termigram.Messages
+{
+	public class DiceMessage : IMessage, ISilentMessage, IReplyMessage, IMarkupMessage
+    {
+		public ChatId? ChatId { get; }
+		public bool DisableNotification { get; }
+		public int ReplyToMessageId { get; }
+		public IReplyMarkup? ReplyMarkup { get; }
+
+		public static readonly DiceMessage Dice = new DiceMessage();
+
+		public DiceMessage(ChatId? chatId = null, bool disableNotification = false, int replyToMessageId = 0, IReplyMarkup? replyMarkup = null)
+		{
+			ChatId = chatId;
+			DisableNotification = disableNotification;
+			ReplyToMessageId = replyToMessageId;
+			ReplyMarkup = replyMarkup;
+		}
+	}
+}
---
+++ Termigram/ResultProcessors/DiceMessageProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Termigram.Bot;
+using Termigram.Commands;
+using Termigram.Extensions;
+using Termigram.Messages;
+
+namespace Termigram.ResultProcessors
+{
+    public class DiceMessageProcessor : ResultProcessorBase<DiceMessage>
+    {
+        protected override Task ProcessResultAsync(IBot bot, ICommand command, DiceMessage result) =>
+            bot.Client.SendDiceAsync(result.ChatId ?? command.Update.GetChatIdOrUserId(), result.DisableNotification, result.ReplyToMessageId, result.ReplyMarkup);
+    }
+}
