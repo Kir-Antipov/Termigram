8d81f599b65a05398daf0706d10c67a781fde401
Kir-Antipov <kp_antipov@mail.ru>
2020-05-23 19:47:42 +0300

    Added `CanConvert` method to IConverter

--- Termigram/Converters/IConverter.cs
+++ Termigram/Converters/IConverter.cs
@@ -4,6 +4,8 @@ namespace Termigram.Converters
 {
     public interface IConverter
     {
+        bool CanConvert(object? value, Type resultType) => TryConvert(value, resultType, out _);
+
         bool TryConvert(object? value, Type resultType, out object? result);
     }
 }
