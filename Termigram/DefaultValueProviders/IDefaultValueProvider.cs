463dab898a6f44c8358a57f9b3e80b265f139425
Kir-Antipov <kp_antipov@mail.ru>
2020-05-23 19:58:32 +0300

    Added `CanProvideDefaultValue` to IDefaultValueProvider

--- Termigram/DefaultValueProviders/IDefaultValueProvider.cs
+++ Termigram/DefaultValueProviders/IDefaultValueProvider.cs
@@ -4,6 +4,8 @@ namespace Termigram.DefaultValueProviders
 {
     public interface IDefaultValueProvider
     {
+        bool CanProvideDefaultValue(ParameterInfo parameter) => TryProvideDefaultValue(parameter, out _);
+
         bool TryProvideDefaultValue(ParameterInfo parameter, out object? value);
     }
 }
