e1b3579b63dfaa5098d6bc189c15c0ecaad1a708
Kir-Antipov <kp_antipov@mail.ru>
2020-05-24 23:20:33 +0300

    Added support of nullable types to DefaultConverter

--- Termigram/Converters/DefaultConverter.cs
+++ Termigram/Converters/DefaultConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Termigram.Converters
 {
@@ -6,11 +7,24 @@ namespace Termigram.Converters
     {
         public override bool TryConvert(object? value, Type resultType, out object? result)
         {
+            result = default;
             try
             {
                 if (resultType.IsAssignableFrom(value?.GetType()))
                 {
                     result = value;
+                    return true;
+                }
+                else if (Nullable.GetUnderlyingType(resultType) is { } underlyingType)
+                {
+                    if (!TryConvert(value, underlyingType, out object? underlyingResult))
+                        return false;
+
+                    ConstructorInfo constructor = resultType.GetConstructor(new[] { underlyingType });
+                    if (constructor is null)
+                        return false;
+
+                    result = constructor.Invoke(new[] { underlyingResult });
                     return true;
                 }
                 else
@@ -21,7 +35,6 @@ namespace Termigram.Converters
             }
             catch
             {
-                result = default;
                 return false;
             }
         }
